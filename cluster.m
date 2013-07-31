d4181183ab4cf3872077affb06284ef8ea1f9239
Zihao Zheng <zihaolucky@gmail.com>
2013-07-31 17:53:05 +0800

    实现了上行、下行空间的可视化

--- cluster.m
+++ cluster.m
@@ -1,23 +1,13 @@
 function cluster(symbol,startvec,endvec,directory)
 
-
+%% Data import
 [Open,High,Low,Close,items]=getData(symbol,startvec,endvec,cd);
 [Short,Med,Long]=SimpleMovingAverage(Close,[5 20 60]);
 [gra_S,gra_M,gra_L]=graMA(Short,Med,Long);
 
-% figure(1)
-% plot(1:items,Close,'r.');
-% hold on
-% plot(1:items,Short,'b-');
-% hold off
 
-
-
-%% 先把UP=[1 2 3 5 56 123],DOWN=[4 7 10 329...]找出来
-%  随后downM,downL,downS...M(UP),L(UP),S(UP)
-%  scatter3(downM,downS,downL,'MarkerEdgeColor','k','MarkerFaceColor',[0 .75 .75])
-%  scatter3(upM,upS,upL,'MarkerEdgeColor','k','MarkerFaceColor',[1 .75 .75])
-
+%% Calculation
+% lowest and highest
 low_index=find(diff(sign(diff(Close)))==2)+1;
 high_index=find(diff(sign(diff(Close)))==-2)+1;
 
@@ -29,7 +19,49 @@ high_S=gra_S(high_index);
 high_L=gra_L(high_index);
 high_M=gra_M(high_index);
 
-%% plot
-scatter3(low_S,low_L,low_M,'MarkerEdgeColor','k','MarkerFaceColor',[0 .75 .75])
+% range
+index=[low_index',high_index'];
+index=sort(index);
+n=size(index,2);
+range=diff(index);
+
+up_index=[];
+down_index=[];
+if index(1)==low_index(1)
+    for i=1:2:n-1
+        up_index=[up_index,index(i):index(i)+range(i)-1];
+    end
+    for i=2:2:n
+        down_index=[down_index,index(i):index(i)+range(i)-1];
+    end
+else
+    for i=1:2:n-1
+        down_index=[down_index,index(i):index(i)+range(i)-1];
+    end
+    for i=2:2:n
+        up_index=[up_index,index(i):index(i)+range(i)-1];
+    end
+end
+
+up_S=gra_S(up_index);
+up_L=gra_L(up_index);
+up_M=gra_M(up_index);
+
+down_S=gra_S(down_index);
+down_L=gra_L(down_index);
+down_M=gra_M(down_index);
+
+%% Plot
+scatter3(low_S,low_M,low_L,'MarkerEdgeColor','k','MarkerFaceColor',[0 .75 .75])
 hold on
-scatter3(high_S,high_L,high_M,'MarkerEdgeColor','k','MarkerFaceColor',[1 .75 .75])
+fprintf('Lowest plotted. Press return to continue.\n\n')
+pause;
+scatter3(high_S,high_M,high_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 .75 .75])
+fprintf('Highest plotted. Press return to continue.\n\n')
+pause;
+scatter3(up_S,up_M,up_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 1 .25])
+fprintf('Up plotted. Press return to continue.\n\n')
+pause;
+scatter3(down_S,down_M,down_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 0 .25])
+fprintf('Down plotted. Press return to continue.\n\n')
+pause;
