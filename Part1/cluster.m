24921b2c6d8dda136cd50bf3de86ab060e40045d
Zihao Zheng <zihaolucky@gmail.com>
2013-08-07 21:44:35 +0800

    cluster实现数据分类的功能

--- Part1/cluster.m
+++ Part1/cluster.m
@@ -1,7 +1,7 @@
-function cluster(symbol,startvec,endvec,directory)
+function [LOW,HIGH,UP,DOWN]=cluster(symbol,t_start,t_end)
 
 %% Data import
-[Open,High,Low,Close,items]=getData(symbol,startvec,endvec,cd);
+[Open,High,Low,Close,items]=getData(symbol,t_start,t_end);
 [Short,Med,Long]=SimpleMovingAverage(Close,[5 20 60]);
 [gra_S,gra_M,gra_L]=graMA(Short,Med,Long);
 
@@ -14,12 +14,14 @@ high_index=find(diff(sign(diff(Close)))==-2)+1;
 low_S=gra_S(low_index);
 low_L=gra_L(low_index);
 low_M=gra_M(low_index);
+LOW=[low_S,low_M,low_L];
 
 high_S=gra_S(high_index);
 high_L=gra_L(high_index);
 high_M=gra_M(high_index);
+HIGH=[high_S,high_M,high_L];
 
-% range
+% up and down
 index=[low_index',high_index'];
 index=sort(index);
 n=size(index,2);
@@ -46,21 +48,29 @@ end
 up_S=gra_S(up_index);
 up_L=gra_L(up_index);
 up_M=gra_M(up_index);
+UP=[up_S,up_M,up_L];
 
 down_S=gra_S(down_index);
 down_L=gra_L(down_index);
 down_M=gra_M(down_index);
+DOWN=[down_S,down_M,down_L];
+
+
 
 %% Plot
 scatter3(low_S,low_M,low_L,'MarkerEdgeColor','k','MarkerFaceColor',[0 .75 .75])
 hold on
 fprintf('Lowest plotted. Press return to continue.\n\n')
-pause;
+%pause;
 scatter3(high_S,high_M,high_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 .75 .75])
 fprintf('Highest plotted. Press return to continue.\n\n')
-pause;
+%pause;
 scatter3(up_S,up_M,up_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 1 .25])
 fprintf('Up plotted. Press return to continue.\n\n')
-pause;
+%pause;
 scatter3(down_S,down_M,down_L,'MarkerEdgeColor','k','MarkerFaceColor',[1 0 .25])
 fprintf('Down plotted. Press return to continue.\n\n')
+
+%% Save data
+filename=['highest_lowest', '.mat'];
+save(filename,'LOW','HIGH');
