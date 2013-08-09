ebd9065b439b89ac4a36095803dc192e9c2bb465
吴亚林 <961773185@qq.com>
2013-08-09 14:13:49 +0800

    Update graMA.m

--- test/graMA.m
+++ test/graMA.m
@@ -3,11 +3,8 @@ function [gra_S,gra_M,gra_L]=graMA(Short,Med,Long)
 %      https://github.com/zihaolucky/Quantitive-Investing
 %
 %% calculation
-Short2=[0;Short(1:end)];
-gra_S=diff(Short2);
+gra_S=diff(Short);
 
-Med2=[0;Med(1:end)];
-gra_M=diff(Med2);
+gra_M=diff(Med);
 
-Long2=[0;Long(1:end)];
-gra_L=diff(Long2);
+gra_L=diff(Long);
