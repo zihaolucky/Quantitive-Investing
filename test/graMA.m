8a51c840e644565085947a53a970ff88760d5803
吴亚林 <961773185@qq.com>
2013-08-09 12:22:22 +0800

    Update graMA.m
    之前得出的数据是实际数据的相反数，而且缺少最后一天的，现在可以了...

--- test/graMA.m
+++ test/graMA.m
@@ -3,14 +3,11 @@ function [gra_S,gra_M,gra_L]=graMA(Short,Med,Long)
 %      https://github.com/zihaolucky/Quantitive-Investing
 %
 %% calculation
-Short2=[0;Short(1:end-1)];
-gra_S=[Short2-Short];
-gra_S=gra_S(1:end-1);
+Short2=[0;Short(1:end)];
+gra_S=diff(Short2);
 
-Med2=[0;Med(1:end-1)];
-gra_M=[Short2-Med2];
-gra_M=gra_M(1:end-1);
+Med2=[0;Med(1:end)];
+gra_M=diff(Med2);
 
-Long2=[0;Long(1:end-1)];
-gra_L=[Long2-Long];
-gra_L=gra_L(1:end-1);
+Long2=[0;Long(1:end)];
+gra_L=diff(Long2);
