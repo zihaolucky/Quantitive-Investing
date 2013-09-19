58f5b1ade26f9409003ddfc968d1224f9791a3d5
Zihao Zheng <zihaolucky@gmail.com>
2013-09-19 23:44:08 +0800

    下载所有上证股票自08年来的数据

---
+++ Part1/getAllData.m
@@ -0,0 +1,16 @@
+function getAllData
+
+A=csvread('stocks.csv');
+for i=1:size(A)
+    symbol=[num2str(A(i)),'.ss'];
+    file=[symbol,'.mat'];
+    if exist(file)
+        fprintf(' %s is existed.\n\n',symbol)
+        continue
+    else
+        tStart=tic;
+        getData(symbol,[2008,1,1],[2013,5,1]);
+        tElapsed=toc(tStart);
+        fprintf(' %s download finished.\n Time: %f\n\n',symbol,tElapsed)
+    end
+end
