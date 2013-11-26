eee27c9ef5480236c3f06315151e72fbcc2249f0
Zihao Zheng <zihaolucky@gmail.com>
2013-11-26 10:05:53 +0800

    debug后的主函数
    使用大样本500进行训练和测试，有较佳模型表现。下一步将与测试模块合并，给出具体的操作表现。

--- cluster_the_sma/ex_svm.m
+++ cluster_the_sma/ex_svm.m
@@ -5,29 +5,40 @@ function ex_svm(symbol,m,n)
 dir_data=[symbol,'.mat'];
 load(dir_data);
 
-Close_trian = Close( 1:m+n+14 );  
-% you want m instance to train and n to test. the last +20 is for MA
+Close = Close( end-m-n-28:end );
 
+% you want m instance to train and n to test. the last +20 is for MA
 
 % Close_test = Close(121:200);
 
-[up_index,down_index,pop_index] = findIndex_up_down(Close_trian);
-[gra_UP,gra_DOWN] = cal_instance(Close_trian, up_index, down_index,m,n);
+[up_index,down_index,pop_index] = findIndex_up_down(Close);
+[gra_UP,gra_DOWN] = cal_instance2(Close, up_index, down_index,m,n);
+
+% how about using the gra_UP(:,1) and gra_UP2(:,3)?
 
 % train model;
 [label, instance] = libsvmread('svm_train.txt');
 [label2, instance2] = libsvmread('svm_test.txt');
 
-model = svmtrain(label, instance, '-c 512 -g 512');
+model = svmtrain(label, instance,'-c 2048 -g 2');
 [predict_label, accuracy, a] = svmpredict(label,instance,model);
 
 [predict_label2, accuracy2, a] = svmpredict(label2,instance2,model);
-result1 = [predict_label, label];
-result2 = [predict_label2, label2];
+result1 = [predict_label, label]
+up_index';
+result2 = [predict_label2, label2]
 
 
 filename=['svm_result.mat'];
 save(filename,'predict_label','label','predict_label2','label2','pop_index')
+
+
+% visualize the result.
+subplot(2,1,1);
+svm_plot(Close,down_index,m);
+
+subplot(2,1,2);
+svm_plot2(Close,label2,m);
 
 
 % Notes:
@@ -35,8 +46,10 @@ save(filename,'predict_label','label','predict_label2','label2','pop_index')
 %       so finally, is ex_svm(symbol,n,m); n,m is the size of training set
 %       and test set.  ----Done!
 %
-%   2. plot.
+%   2. plot. There still has some bugs.
 %
 %
 %   3. change the character. To add -1/1 signal.
 %       so the size of instance would be 4,5..
+%                      ----Done! see cal_instance2.m
+%
